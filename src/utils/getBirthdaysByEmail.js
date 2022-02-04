80012934d7a242b65c4e5fe397ed0e92d4c1266e
subhasis020299 <subhasisdas125@gmail.com>
2022-02-04 16:54:10 +0530

    this should work

--- src/utils/getBirthdaysByEmail.js
+++ src/utils/getBirthdaysByEmail.js
@@ -10,28 +10,28 @@ module.exports = () => {
 		.connect()
 		.then(() => {
 			console.log("Connected to DB");
+			const db = client.db(process.env.DB_NAME);
+
+			return new Promise((resolve, reject) => {
+				const date = new Date();
+				const day = date.getDate();
+				const month = date.getMonth() + 1;
+
+				db.collection("birthdays")
+					.aggregate([
+						{ $match: { month, day } },
+						{ $group: { _id: "$email", birthdays: { $addToSet: "$friend" } } },
+					])
+					.toArray()
+					.then((data) => {
+						console.log("Mail Payload:", data);
+						resolve(data);
+					})
+					.catch((err) => reject(err));
+			});
 		})
 		.catch((error) => {
 			console.error("Error connecting to DB", error);
+			return;
 		});
-
-	const db = client.db(process.env.DB_NAME);
-
-	return new Promise((resolve, reject) => {
-		const date = new Date();
-		const day = date.getDate();
-		const month = date.getMonth() + 1;
-
-		db.collection("birthdays")
-			.aggregate([
-				{ $match: { month, day } },
-				{ $group: { _id: "$email", birthdays: { $addToSet: "$friend" } } },
-			])
-			.toArray()
-			.then((data) => {
-				console.log("Mail Payload:", data);
-				resolve(data);
-			})
-			.catch((err) => reject(err));
-	});
 };
