ad1cb80675c3c5b4c804b0236826d99fb48e3aa1
subhasis020299 <subhasisdas125@gmail.com>
2022-02-04 16:03:41 +0530

    added check to see if there are any reminders

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ const sendEmail = require("./utils/sendEmail");
 module.exports.sendReminderEmail = async () => {
 	try {
 		const birthdayReminders = await getBirthdaysByEmail();
-		birthdayReminders.forEach((reminder) => {
-			const { email, birthdays } = reminder;
-			sendEmail(email, birthdays);
-		});
+		if (birthdayReminders.length > 0) {
+			birthdayReminders.forEach((reminder) => {
+				const { email, birthdays } = reminder;
+				sendEmail(email, birthdays);
+			});
 
-		return "Email(s) Sent!";
+			return "Email(s) Sent!";
+		} else {
+			return "No Birthdays to send!";
+		}
 	} catch (error) {
 		console.error(error);
 		return "sendReminderEmail failed!";
